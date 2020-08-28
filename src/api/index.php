93047ffe49d97bd509670603c54c9744090594eb
nvande <t2vanden@gmail.com>
2020-08-28 15:29:45 -0500

    remove debug flag

--- src/api/index.php
+++ src/api/index.php
@@ -1,11 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 /**
- * This is a template php file for your countries search.
- * Use as you will, or start over. It's up to you.
+ * This php server opens a curl tunnel to the REST countries API,
+ * performs a search action by either name, full name, or country code,
+ * and sorts the response by population. The data is returned as JSON.
  */
-
-$debug = false;
 
 // Define base REST Country API urls 
 
@@ -17,7 +16,6 @@ $query_str_fullname = '?fullText=true';
 
 // endpoint for searching by country code
 $url_code = 'https://restcountries.eu/rest/v2/alpha';
-
 
 // Define the possible query keys for this server
 $codeQuery = 'code';
@@ -31,25 +29,12 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $query_str = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
 parse_str($query_str, $query_params);
 
-if($debug) {
-    // Debug dump for query string
-    print_r("URI:  ".$uri."\n");
-    print_r("Query String:  ".$query_str."\n");
-    print_r($query_params);
-
-}
-
 // Boolean for search by country code, otherwise it will be by name
 $code = (array_key_exists($codeQuery, $query_params) && $query_params[$codeQuery] == 'true') ? 1 : 0;
 
 // Boolean for search by full name
 // this will be ignored if we are searching by country code
 $fullName = (array_key_exists($fullNameQuery, $query_params) && $query_params[$fullNameQuery] == 'true') ? 1 : 0;
-
-if($debug) {
-    print_r("Country Code: ".$code."\n");
-    print_r("Full Country Name: ".$fullName."\n");
-}
 
 if(!$code){
     $query = $fullName ? $query_str_fullname : '';
@@ -58,13 +43,6 @@ if(!$code){
 else {
     $url = $url_code . $uri;
 }
-
-if($debug) {
-    // output the final url used for our API call
-    print_r($url."\n");
-    echo "\n";
-}
-
 
 // Open Curl Tunnel
 $ch = curl_init();
